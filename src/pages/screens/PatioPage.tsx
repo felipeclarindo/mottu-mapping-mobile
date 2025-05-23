bd15c906a649e609fa7ca3a612ee5a03df66be99
felipeclarindo <fglpc2@gmail.com>
2025-05-23 17:41:25 -0300

    refactor(PatioPage): Refactor the PatioPage screen to improve the interface.

--- src/pages/screens/PatioPage.tsx
+++ src/pages/screens/PatioPage.tsx
@@ -1,77 +1,87 @@
 import React from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
 import MotoCard from "../../components/MotoCard";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import { useNavigation, DrawerActions } from "@react-navigation/native";
 import { Moto } from "../../types";
 
+const motos: Moto[] = [
+  {
+    id: "M1",
+    setorName: "OK",
+    setorDescription: "OK",
+    setorColorRgb: "#00FF00",
+    plate: "ABC1234",
+    setorId: "1",
+  },
+  {
+    id: "M2",
+    setorName: "Em Manutenção",
+    setorDescription: "Em manutenção",
+    setorColorRgb: "#FFA500",
+    plate: "DAC1263",
+    setorId: "2",
+  },
+  {
+    id: "M3",
+    setorName: "Pronta",
+    setorDescription: "Pronta para uso",
+    setorColorRgb: "#0000FF",
+    plate: "DAC1534",
+    setorId: "3",
+  },
+  {
+    id: "M4",
+    setorName: "OK",
+    setorDescription: "OK",
+    setorColorRgb: "#00FF00",
+    plate: "ADA123",
+    setorId: "1",
+  },
+  {
+    id: "M5",
+    setorName: "Em Manutenção",
+    setorDescription: "Em manutenção",
+    setorColorRgb: "#FFA500",
+    plate: "ADA1313",
+    setorId: "2",
+  },
+  {
+    id: "M6",
+    setorName: "Pronta",
+    setorDescription: "Pronta para uso",
+    setorColorRgb: "#0000FF",
+    plate: "Azul",
+    setorId: "3",
+  },
+  {
+    id: "M7",
+    setorName: "OK",
+    setorDescription: "OK",
+    setorColorRgb: "#00FF00",
+    plate: "ASG1241",
+    setorId: "1",
+  },
+  {
+    id: "M8",
+    setorName: "Em Manutenção",
+    setorDescription: "Em manutenção",
+    setorColorRgb: "#FFA500",
+    plate: "ADS5113",
+    setorId: "2",
+  },
+  {
+    id: "M9",
+    setorName: "Pronta",
+    setorDescription: "Pronta para uso",
+    setorColorRgb: "#0000FF",
+    plate: "ASG1241",
+    setorId: "3",
+  },
+];
+
 const PatioPage = () => {
-  const motos: Moto[] = [
-    {
-      id: "M1",
-      setorName: "OK",
-      setorDescription: "OK",
-      setorColorRgb: "#00FF00",
-      setorId: "1",
-    },
-    {
-      id: "M2",
-      setorName: "Em Manutenção",
-      setorDescription: "Em manutenção",
-      setorColorRgb: "#FFA500",
-      setorId: "2",
-    },
-    {
-      id: "M3",
-      setorName: "Pronta",
-      setorDescription: "Pronta para uso",
-      setorColorRgb: "#0000FF",
-      setorId: "3",
-    },
-    {
-      id: "M4",
-      setorName: "OK",
-      setorDescription: "OK",
-      setorColorRgb: "#00FF00",
-      setorId: "1",
-    },
-    {
-      id: "M5",
-      setorName: "Em Manutenção",
-      setorDescription: "Em manutenção",
-      setorColorRgb: "#FFA500",
-      setorId: "2",
-    },
-    {
-      id: "M6",
-      setorName: "Pronta",
-      setorDescription: "Pronta para uso",
-      setorColorRgb: "#0000FF",
-      setorId: "3",
-    },
-    {
-      id: "M7",
-      setorName: "OK",
-      setorDescription: "OK",
-      setorColorRgb: "#00FF00",
-      setorId: "1",
-    },
-    {
-      id: "M8",
-      setorName: "Em Manutenção",
-      setorDescription: "Em manutenção",
-      setorColorRgb: "#FFA500",
-      setorId: "2",
-    },
-    {
-      id: "M9",
-      setorName: "Pronta",
-      setorDescription: "Pronta para uso",
-      setorColorRgb: "#0000FF",
-      setorId: "3",
-    },
-  ];
   const navigation = useNavigation();
 
   return (
@@ -80,20 +90,24 @@ const PatioPage = () => {
         title="Pátio de Motos"
         onMenuPress={() => navigation.dispatch(DrawerActions.openDrawer())}
       />
-      <ScrollView contentContainerStyle={styles.content}>
-        <View style={styles.grid}>
-          {motos.map((moto) => (
-            <MotoCard
-              key={moto.id}
-              id={moto.id}
-              setorName={moto.setorName}
-              setorDescription={moto.setorDescription}
-              setorColorRgb={moto.setorColorRgb}
-              setorId={moto.setorId}
-            />
-          ))}
-        </View>
-      </ScrollView>
+
+      <FlatList
+        data={motos}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+        columnWrapperStyle={styles.row}
+        contentContainerStyle={styles.content}
+        renderItem={({ item }) => (
+          <MotoCard
+            id={item.id}
+            setorName={item.setorName}
+            setorDescription={item.setorDescription}
+            setorColorRgb={item.setorColorRgb}
+            setorId={item.setorId}
+          />
+        )}
+      />
+
       <Footer />
     </View>
   );
@@ -106,13 +120,10 @@ const styles = StyleSheet.create({
   },
   content: {
     padding: 20,
-    flexGrow: 1,
   },
-  grid: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    gap: 12,
+  row: {
+    justifyContent: "space-between",
+    marginBottom: 12,
   },
 });
 
