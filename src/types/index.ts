0a17479a6cbdd7e314682c5638a416cb5b655353
felipeclarindo <fglpc2@gmail.com>
2025-05-23 03:04:31 -0300

    feat(types): Implements props to use in pages and components.

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+import { DrawerNavigationProp } from "@react-navigation/drawer";
+import { ImageSourcePropType } from "react-native";
+
+export type DrawerParamList = {
+  home: undefined;
+  patio: undefined;
+  report: undefined;
+  login: undefined;
+};
+
+export type UserProps = {
+  user: string;
+  email: string;
+  senha?: string;
+};
+
+export type HomeScreenNavigationProp = DrawerNavigationProp<
+  DrawerParamList,
+  "home"
+>;
+
+export type Moto = {
+  id: string;
+  setorDescription: string;
+  setorName: string;
+  setorColorRgb: string;
+  setorId: string;
+};
+
+export type ImageModalProps = {
+  visible: boolean;
+  onClose: () => void;
+  imageSource: ImageSourcePropType;
+};
+
+export type DashboardCardProps = {
+  stats: {
+    [key: string]: number;
+  };
+};
