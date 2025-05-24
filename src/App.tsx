ceb46f9ab350d5231c43c4c7e2debaa729f79aa6
felipeclarindo <fglpc2@gmail.com>
2025-05-24 18:54:46 -0300

    feat(app): Include the mainNavigator to use in the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,6 @@
-// App.tsx
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Platform, StatusBar } from "react-native";
-import AppStack from "./navigation/AppStack";
+import MainNavigator from "./navigation/MainNavigator";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <SafeAreaView style={styles.safeArea}>
-        <ExpoStatusBar translucent backgroundColor="transparent" style="dark" />
-        <AppStack />
-      </SafeAreaView>
-    </NavigationContainer>
-  );
+  return <MainNavigator />;
 }
-
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    padding: 0,
-    margin: 0,
-    backgroundColor: "#fff",
-  },
-});
