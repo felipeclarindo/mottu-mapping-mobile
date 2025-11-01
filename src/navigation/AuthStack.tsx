f11158c7bb5e59353aa27e78641ae6b9c9a70752
andremarko <151890840+andremarko@users.noreply.github.com>
2025-11-01 19:23:56 -0300

    refactor: remove RegisterView e ajusta path LoginView

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginView from "../view/auth/LoginView";
-import RegisterView from "../view/auth/RegisterView";
+import LoginView from "../view/LoginView";
 import DrawerNavigator from "./DrawerNavigator";
 
 const Stack = createNativeStackNavigator();
@@ -10,7 +9,6 @@ export default function AuthStack() {
   return (
     <Stack.Navigator id={undefined} screenOptions={{ headerShown: false }}>
       <Stack.Screen name="login" component={LoginView} />
-      <Stack.Screen name="register" component={RegisterView} />
       <Stack.Screen name="drawer" component={DrawerNavigator} />
     </Stack.Navigator>
   );
