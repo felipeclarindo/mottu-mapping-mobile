cb541d58ae40c990c9a65eb8b6c6c0ea7bb64974
andremarko <151890840+andremarko@users.noreply.github.com>
2025-11-01 19:22:48 -0300

    feat: adiciona url do API parametrizada apontando para expoConfig

--- src/fetcher/api.ts
+++ src/fetcher/api.ts
@@ -1,7 +1,18 @@
 import axios from "axios";
+import * as SecureStore from "expo-secure-store";
 
-const API_URL = "http://localhost:8080/api";
-
+import Constants from "expo-constants";
+const API_URL = Constants.expoConfig?.extra?.API_URL || "";
 export const mottuMappingApi = axios.create({
   baseURL: API_URL,
+});
+
+mottuMappingApi.interceptors.request.use(async (config) => {
+  const token = await SecureStore.getItemAsync("jwt_token");
+  if (token && config.headers) {
+    (config.headers as Record<string, string>)[
+      "Authorization"
+    ] = `Bearer ${token}`;
+    return config;
+  }
 });
