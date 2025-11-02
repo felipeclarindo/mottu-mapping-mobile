992e1cef0a703735845206ee8d9e41928a203a22
andremarko <151890840+andremarko@users.noreply.github.com>
2025-11-01 22:16:26 -0300

    feat: adiciona provider (context), statusbar hidden

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import MainNavigator from "./navigation/MainNavigator";
+import { AuthProvider } from "./context/AuthContext";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
-  return <MainNavigator />;
+  return (
+    <AuthProvider>
+      <StatusBar hidden />
+      <MainNavigator />
+    </AuthProvider>
+  );
 }
